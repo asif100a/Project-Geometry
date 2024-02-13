9681317e56aab400d44b41601574ad94711fdf4b
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-02-13 12:42:06 +0600

    get Rectangle Length value

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,3 +1,14 @@
 function calculateRectangleArea() {
-    console.log('connect rect function');
-}
+    // get Rectangle Width value
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const rectangleWidth = parseFloat(rectangleWidthText);
+    // console.log(typeof rectangleWidth);
+
+    // get Rectangle Length value
+    const rectangleLengthInput = document.getElementById('rectangle-lenght');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const rectangleLength = parseFloat(rectangleLengthText);
+    
+    // get calculation The Rectangle Area
+};
